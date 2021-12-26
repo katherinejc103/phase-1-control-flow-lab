21d5bf1b31ec39165e9fdf5df5f24a37d7ba836a
Katherine Jean-Charles <katherinejc103@gmail.com>
2021-12-26 10:10:08 -0500

    Done

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if 
+    (distance <= 2500 && distance > 400) {
+    return 'I will gladly take your thirty bucks.';
+  } else { 
+    return 'No can do.';
+}}
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) 
+  console.log(tip)
+switch(tip) {
+  case generousTip
+  FileSystem.out.println("Thank you so much. ");
+  break;
+  case notAsGenerousTip
+  FileSystem.out.println("Thank you. ");
+  break;
+  default:
+    FileSystem.out.println("Bye. ");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+ // string switchOnCharmFromTip(int tip){
+ // switch(tip)S
+  // case generousTip: console.log('Thank you so much.');
+  // break;
+  // case notAsGenerousTip: console.log('Thank you.');
+  // break;
+  // default:
+  // console.log('Bye.');
+
+  
